52b82ab047ba8c408f937963f2e63c26553c747a
SubhranilRaha <82709110+SubhranilRaha@users.noreply.github.com>
2023-04-16 22:35:44 +0530

    first

---
+++ src/main/java/com/subhranil/kafkaprac/config/KafkaConfigProps.java
@@ -0,0 +1,19 @@
+package com.subhranil.kafkaprac.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Configuration
+@ConfigurationProperties(prefix = "subhranil.kafkaprac")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class KafkaConfigProps {
+    private String topic;
+}
---
+++ src/test/java/com/subhranil/kafkaprac/KafkaPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhranil.kafkaprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
